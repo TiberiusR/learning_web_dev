cf498b568d9ce78a6feac792d03de34b4fb16a6c
TiberiusR <tiberius.r@hotmail.com>
2020-08-22 22:25:38 -0300

    time for javascript

---
+++ Javascript/calc.js
@@ -0,0 +1,6 @@
+let age = prompt("What's your age (in years)?");
+
+age *= 365.25;
+
+alert("You've been alive for " + age + " days!");
+console.log(age);
---
+++ Javascript/script.js
@@ -0,0 +1,8 @@
+let firstName = prompt("what's your first name?");
+
+let lastName = prompt("what's your last name?");
+
+let age = prompt("what's your age?");
+
+alert("Hello there " + firstName + " " + lastName + "!");
+alert("You're " + age + " years old" + "!");

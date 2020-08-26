5b274bcb2a769d808aea25a008d705de0f0005dd
TiberiusR <tiberius.r@hotmail.com>
2020-08-26 10:49:45 -0300

    DOM exercises

---
+++ DOM/domdemo.js
@@ -0,0 +1,9 @@
+let qh1 = document.querySelector("h1");
+
+let p1 = document.getElementById("first");
+
+let p11 = document.querySelector("#first");
+
+let p111 = document.querySelector(".special");
+
+let p1111 = document.getElementsByClassName("special")[0];

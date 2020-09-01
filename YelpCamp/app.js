d94f083d17fcd7a0722a835f4331f3a31a4e9079
TiberiusR <tiberius.r@hotmail.com>
2020-09-01 18:07:41 -0300

    start of yelpcamp

---
+++ YelpCamp/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("landing");
+})
+
+let campgrounds = [
+    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name: "Mountain Goat", image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+]
+
+app.get("/campgrounds", function(req, res) {
+    res.render("campgrounds", {campgrounds: campgrounds});
+})
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new");
+})
+
+app.post("/campgrounds", function(req, res) {
+    //get data from form and add to array
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    //redirect back to page
+    res.redirect("/campgrounds");
+})
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
+});

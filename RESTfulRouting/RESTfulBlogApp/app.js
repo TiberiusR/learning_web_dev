16baa7dd3f76eb9b2afa4c60e04f2d3a4a7e9b77
TiberiusR <tiberius.r@hotmail.com>
2020-09-02 22:55:46 -0300

    starting to look like REST

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -25,8 +25,9 @@ const Blog = mongoose.model("Blog", blogSchema);
 
 app.get("/", function(req, res) {
     res.redirect("/blogs");
-})
+});
 
+//INDEX ROUTE
 app.get("/blogs", function(req, res) {
     Blog.find({}, function(err, blogs) {
         if(err) {
@@ -37,6 +38,32 @@ app.get("/blogs", function(req, res) {
     });
 });
 
+//NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+})
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res) {
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if(err) {
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
 
 
 
